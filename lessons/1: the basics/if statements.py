e9b8230704259f118cf1b97aa4b81381949f8947
Abdullah Ihsan <ihsanabdullah2004@gmail.com>
2024-09-12 22:56:47 +0100

    a

---
+++ lessons/1: the basics/if statements.py
@@ -0,0 +1,24 @@
+age = int(input("how old are you?"))
+
+if (age < 18):
+    print("you are not an adult by western standards...")
+
+if age > 18 :
+    print("you are an adult by western standards...")
+
+if age == 18:
+    print("you just turned into an adult by western standards")
+
+weight = int(input("how many kg do you weigh?"))
+
+if (weight >= 150):
+    print("Akhi im worried for your health. you got to hit gym asap and lose some weight!!!")
+
+if (weight <= 50):
+    print("Akhi im worried for your health. you got to hit gym asap and put on some muscle!!!")
+
+favFood = input("whats ur favourite food? ")
+
+if (favFood == "burger"):
+    print("you have good taste")
+    print("we should go out to eat some time. i heard smash burger is really good")
