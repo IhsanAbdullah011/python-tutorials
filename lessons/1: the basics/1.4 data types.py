c2ffdf9a5c51cdb86390d8a6e8112d17a8100fdf
Abdullah Ihsan <ihsanabdullah2004@gmail.com>
2024-09-13 15:29:36 +0100

    f

---
+++ lessons/1: the basics/1.4 data types.py
@@ -0,0 +1,24 @@
+#String -> str
+string = "text"
+
+#integer -> int
+wholeNumber = 10
+
+#float -> float
+decimal = 12.5
+
+#boolean = bool
+isMale = True #holds a true or false value
+
+#list
+shoppingList = ["apple","bannana","pineapple","pizza"]
+
+"""
+these are hte most common data types you will interact with.
+the computer will recognise them to be different from each other and therefore you can use them differently
+for instance you can do maths with integers but not with strings, you can concatinate with strings but not with integers,
+lists can store different bits of information separated by a ',', booleans hold true or false values etc...
+
+this was jsut an intro and basic explenation of the various data types we may deal with. as we progress we will look at
+their uses later and when and why we may use them
+"""
